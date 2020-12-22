2ce2a879fb8a7f45eba69e3ec05cc60835699130
Rafael Barros <rafa.a.barros@hotmail.com>
2020-12-22 13:52:51 -0300

    reorganizando estrutura do projeto

--- functions/index.js
+++ functions/index.js
@@ -1,81 +1,25 @@
-const express = require("express")
-const {ApolloServer, gql} = require("apollo-server-express")
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-const serviceAccount = require('./fiapbancographql-firebase-adminsdk-b531i-8746c847e2.json')
+const serviceAccount = require("./banco-rafael-firebase.json");
+
+const resolvers = require("./resolvers");
+const { importSchema } = require("graphql-import");
 
 admin.initializeApp({
-    credential:admin.credential.cert(serviceAccount),
-    databaseURL:"https://fiapbancographql-default-rtdb.firebaseio.com/"
-})
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fiapbancographql-rafael-default-rtdb.firebaseio.com/",
+});
 
-const typeDefs = gql`
-    type Produto{
-        id:Int
-        nomeproduto:String
-        descricao:String
-        fornecedor:String
-        preco:Float
-        datacadastro:String
-    }
-    type Query{
-        produto:[Produto]
-    }
-    type Mutation{
-        novoProduto(
-            id:Int
-            nomeproduto:String
-            descricao:String
-            fornecedor:String
-            preco:Float
-            datacadastro:String
-        ):[Produto]
-
-    }
-`
-const resolvers = {
-    Query:{
-        produto:()=>{
-            return admin.database()
-                   .ref("produtos")
-                   .once("value")
-                   .then(snap => snap.val())
-                   .then(val => Object.keys(val)
-                   .map((key)=>val[key]))
-        }
-    },
-    Mutation:{
-        novoProduto(_,{id,nomeproduto,descricao,fornecedor,preco,datacadastro}){
-            const novo = {
-            id:id,
-            nomeproduto:nomeproduto,
-            descricao:descricao,
-            fornecedor:fornecedor,
-            preco:preco,
-            datacadastro:datacadastro
-        }
-        admin.database()
-        .ref("produtos")
-        .push(novo)
-        
-        return admin.database()
-                   .ref("produtos")
-                   .limitToLast(1)
-                   .once("value")
-                   .then(snap => snap.val())
-                   .then(val => Object.keys(val)
-                   .map((key)=>val[key]))
-    }
-}
-}
-
-const app = express()
+const app = express();
 
 const server = new ApolloServer({
-    typeDefs,resolvers
-})
+  typeDefs: importSchema("./schema/index.graphql"),
+  resolvers: resolvers,
+});
 
-server.applyMiddleware({app,path:"/",cors:true})
+server.applyMiddleware({ app, path: "/", cors: true });
 
-exports.graphql = functions.https.onRequest(app)
+exports.graphql = functions.https.onRequest(app);
---
+++ functions/resolvers/index.js
@@ -0,0 +1,3 @@
+const Produto = require("./Produto");
+
+module.exports = Produto;
